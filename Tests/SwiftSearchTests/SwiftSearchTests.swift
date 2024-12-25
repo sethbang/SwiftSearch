2a9dd4b57a7341f4257c0474954294ddda7878e6
Seth Bangert <95317728+sethbang@users.noreply.github.com>
2024-12-25 04:03:58 -0700

    chore: add test infrastructure
    - Add test target to Package.swift
    - Create SwiftSearchTests directory and initial test file
    - Set up @testable import for test access

---
+++ Tests/SwiftSearchTests/SwiftSearchTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import SwiftSearch
+
+final class SwiftSearchTests: XCTestCase {
+    func testExample() throws {
+        // This is an example test case.
+        XCTAssertTrue(true)
+    }
+}
